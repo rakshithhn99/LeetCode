9b0f9990e0901cef73f26d9027112492da8df6cc
Rakshitha H N <57651892+rakshithhn99@users.noreply.github.com>
2024-02-26 19:08:26 +0530

    Time: 581 ms (73.96%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,20 +1,14 @@
 # Write your MySQL query statement below
 
-SELECT T.name FROM (
-SELECT A.id, A.name, COUNT(A.id) as reportcount FROM 
-Employee A INNER JOIN Employee B
-ON A.id = B.managerId
-GROUP BY
-A.id,
-A.name ) T
-WHERE T.reportcount >= 5;
+WITH CTE AS (
+SELECT 
+managerId, 
+COUNT(*) AS numOfMang 
+FROM Employee 
+WHERE managerId IS NOT NULL
+GROUP BY managerId
+HAVING numOfMang >=5)
 
-
-/* Explanation : 
-Since input is only one table Employee table that has both employee and manager Id details. 
-
-We should treat same table as employee and manager perspective. So join id with managerId. 
-
-Here while selecting either we can select A.id or B.managerId but selecting name we should select left table name since right table name keep on changes(employee name). 
-Rest is self explanatory..
-*/
+SELECT name 
+FROM Employee INNER JOIN CTE 
+ON Employee.id = CTE.managerId;
